380a31f1508d49f5c816b8dcf07bfccc46e38357
Marc d'Avernas <marc.davernas@gmail.com>
2016-08-04 23:09:44 -0400

    changing keys again

--- test2.py
+++ test2.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.keys import Keys
 driver = webdriver.PhantomJS()
 driver.set_window_size(1120, 550)
 driver.get("https://duckduckgo.com/")
-driver.find_element_by_id('search_form_input_homepage').send_keys("realpython")
+driver.find_element_by_id('search_form_input_homepage').send_keys("key")
 driver.find_element_by_id("search_button_homepage").click()
 print driver.current_url
 driver.quit()
